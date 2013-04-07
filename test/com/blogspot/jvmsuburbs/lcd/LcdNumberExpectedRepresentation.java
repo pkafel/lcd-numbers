0043da95bd7ee1c7a761a91a05a9b694e0198515
pkafel <piotr.kafel@gmail.com>
2013-04-07 19:31:19 +0200

    Add expected numbers representations in order to avoid duplications

---
+++ test/com/blogspot/jvmsuburbs/lcd/LcdNumberExpectedRepresentation.java
@@ -0,0 +1,24 @@
+package com.blogspot.jvmsuburbs.lcd;
+
+public class LcdNumberExpectedRepresentation {
+
+	public static final String zero =  " _ \n" + "| |\n" + "|_|\n";
+	
+	public static final String one =   "   \n" + "  |\n" + "  |\n";
+	
+	public static final String two =   " _ \n" + " _|\n" + "|_ \n";
+	
+	public static final String three = " _ \n" + " _|\n" + " _|\n";
+	
+	public static final String four =  "   \n" + "|_|\n" + "  |\n";
+	
+	public static final String five =  " _ \n" + "|_ \n" + " _|\n";
+	
+	public static final String six =   " _ \n" + " _|\n" + "|_|\n";
+	
+	public static final String seven = " _ \n" + "  |\n" + "  |\n";
+	
+	public static final String eight = " _ \n" + "|_|\n" + "|_|\n";
+	
+	public static final String nine =  " _ \n" + "|_|\n" + " _|\n";
+}
