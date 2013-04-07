4550d4271bf608560e760d9608d5a3f3175787ac
pkafel <piotr.kafel@gmail.com>
2013-04-07 18:47:15 +0200

    Passing test with representation of 2

--- src/com/blogspot/jvmsuburbs/lcd/LcdNumberGenerator.java
+++ src/com/blogspot/jvmsuburbs/lcd/LcdNumberGenerator.java
@@ -2,9 +2,18 @@ package com.blogspot.jvmsuburbs.lcd;
 
 public class LcdNumberGenerator {
 
-	private static final String one = "   \n" + "  |'n" + "  |"; 
+	private static final String one = "   \n" + "  |'n" + "  |";
+	private static final String two = " _ \n" + " _|\n" + "|_ "; 
 	
-	public static String generateRepresentationFor(String string) {
-		return one;
+	public static String generateRepresentationFor(String numberToRepresent) {
+
+		if (numberToRepresent.equals("1")) {
+			
+			return one;
+			
+		} else {
+			
+			return two;
+		}
 	}
 }
