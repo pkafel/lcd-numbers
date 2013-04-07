f0cb4a9fc01d866f34daf01585736dda8c1d1ab3
pkafel <piotr.kafel@gmail.com>
2013-04-07 21:18:22 +0200

    Add main function

---
+++ src/com/blogspot/jvmsuburbs/lcd/LcdMain.java
@@ -0,0 +1,9 @@
+package com.blogspot.jvmsuburbs.lcd;
+
+public class LcdMain {
+
+	public static void main(String[] args) {
+		
+		System.out.println(LcdNumberGenerator.generateRepresentationFor("2"));
+	}
+}
