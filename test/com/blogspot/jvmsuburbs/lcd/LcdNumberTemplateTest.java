1a120917f025e57497565cff13e559b2e4ba6865
pkafel <piotr.kafel@gmail.com>
2013-04-08 22:09:47 +0200

    Add test for concatenating templates

--- test/com/blogspot/jvmsuburbs/lcd/LcdNumberTemplateTest.java
+++ test/com/blogspot/jvmsuburbs/lcd/LcdNumberTemplateTest.java
@@ -8,10 +8,9 @@ public class LcdNumberTemplateTest {
 	@Test
 	public void shouldBuildCorrectTemplateWithSize1(){
 		// Given
-		int size = 1;
 		
 		// When
-		LcdNumberTemplate template = new LcdNumberTemplate.Builder(size).build();
+		LcdNumberTemplate template = new LcdNumberTemplate.Builder().build();
 		
 		// Then
 		Assert.assertEquals(LcdNumberExpectedRepresentation.eight, template.toString());
@@ -20,14 +19,35 @@ public class LcdNumberTemplateTest {
 	@Test
 	public void shouldBuildAnotherCorrectTemplateWithSize1(){
 		// Given
-		int size = 1;
 		
 		// When
-		LcdNumberTemplate template = new LcdNumberTemplate.Builder(size)
+		LcdNumberTemplate template = new LcdNumberTemplate.Builder()
 				.withoutMiddleLeftSide().withoutMiddleCenterSide()
 				.withoutDownLeftSide().withoutDownCenterSide().build();
 		
 		// Then
 		Assert.assertEquals(LcdNumberExpectedRepresentation.seven, template.toString());
+	}
+	
+	@Test
+	public void shouldAddTwoTemplates(){
+		// Given
+		LcdNumberTemplate.Builder builder = new LcdNumberTemplate.Builder();
+		LcdNumberTemplate t1 = builder.withoutTopCenterSide()
+				.withoutTopCenterSide().withoutMiddleLeftSide()
+				.withoutMiddleCenterSide().withoutDownLeftSide()
+				.withoutDownCenterSide().build();
+		LcdNumberTemplate t2 = builder.withoutMiddleLeftSide()
+				.withoutDownRightSide().build();
+		LcdNumberTemplate t3 = builder.build();
+
+		// When
+		LcdNumberTemplate template = t1.concatenateNumber(t2).concatenateNumber(t3);
+		
+		// Then
+		String correctRepresentation = "   " + " _ " + " _ \n" +
+				   					   "  |" + " _|" + "|_|\n" +
+				   					   "  |" + "|_ " + "|_|\n";
+		Assert.assertEquals(correctRepresentation, template.toString());
 	}
 }
