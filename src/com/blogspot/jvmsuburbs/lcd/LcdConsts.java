08452fd221f8bfebd300f3d75daa5a10e2093ce4
pkafel <piotr.kafel@gmail.com>
2013-04-14 12:09:12 +0200

    Add class with constants

---
+++ src/com/blogspot/jvmsuburbs/lcd/LcdConsts.java
@@ -0,0 +1,13 @@
+package com.blogspot.jvmsuburbs.lcd;
+
+public final class LcdConsts {
+
+	private LcdConsts() {
+	}
+	
+	public static final char empty = ' ';
+	
+	public static final char horizontal = '_';
+	
+	public static final char vertical = '|';
+}
